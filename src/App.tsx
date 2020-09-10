40610caa7401c06fd89cf14856a35dce3302685d
jpomykala <jakub.pomykala@gmail.com>
2020-09-10 20:04:44 +0200

    Init

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,37 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { useTranslation } from "react-i18next";
 
 function App() {
+
+  const { t, i18n } = useTranslation();
+
   return (
     <div className="App">
       <header className="App-header">
+        <div>
+          <p>
+            {t("USE_BUTTONS_BELOW")}
+          </p>
+          <button onClick={() => i18n.changeLanguage("en")}>English</button>
+          <button onClick={() => i18n.changeLanguage("es")}>Spanish</button>
+          <button onClick={() => i18n.changeLanguage("pl")}>Polish</button>
+          <hr/>
+        </div>
+
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          {t("DESCRIPTION")}
         </p>
+
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://simplelocalize.io"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          {t("LEARN_MORE")}
         </a>
       </header>
     </div>
--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,16 @@
-import React from 'react';
+import React, { Suspense } from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import './i18n';
+
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Suspense fallback={<div>Loading...</div>}>
+      <App />
+    </Suspense>
   </React.StrictMode>,
   document.getElementById('root')
 );
