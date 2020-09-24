84f0190320ad646ca2594cdda818046ae6cc472a
jpomykala <jakub.pomykala@gmail.com>
2020-09-24 15:48:52 +0200

    Adjust configuration

--- src/i18n.ts
+++ src/i18n.ts
@@ -3,8 +3,8 @@ import Backend from 'i18next-http-backend'
 import LanguageDetector from 'i18next-browser-languagedetector'
 import { initReactI18next } from 'react-i18next'
 
-const apiKey = "5e13e3019cff4dc6abe36009445f0883";
-const loadPath = `https://api.simplelocalize.io/api/v1/i18next/translations/{{lng}}/{{ns}}?api_key=${apiKey}`;
+const project_token = "5e13e3019cff4dc6abe36009445f0883";
+const loadPath = `https://api.simplelocalize.io/api/v1/i18next/translations/{{lng}}/{{ns}}?project_token=${project_token}`;
 
 i18n
   // Enables the i18next backend
