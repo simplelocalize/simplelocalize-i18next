99e7de3cce5cfe65ee6a9942eefe68cc75e5549d
jpomykala <jakub.pomykala@gmail.com>
2020-10-01 19:37:58 +0200

    Updates

---
+++ precache-manifest.ee5c57d3c3896ed65a4f03139109ed03.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eb6905d0f6a422cf9fdc889d9a299744",
+    "url": "/simplelocalize-i18next/index.html"
+  },
+  {
+    "revision": "8f44710730d4d1864337",
+    "url": "/simplelocalize-i18next/static/css/main.d7edba43.chunk.css"
+  },
+  {
+    "revision": "2ad60bdb9d3d9ae4a9c7",
+    "url": "/simplelocalize-i18next/static/js/2.1e017523.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/simplelocalize-i18next/static/js/2.1e017523.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8f44710730d4d1864337",
+    "url": "/simplelocalize-i18next/static/js/main.836a26ca.chunk.js"
+  },
+  {
+    "revision": "4939c6daee2bbd78017a",
+    "url": "/simplelocalize-i18next/static/js/runtime-main.51b7db53.js"
+  },
+  {
+    "revision": "013f0b90a4cae7b8bbf13e3fd9e7dc4a",
+    "url": "/simplelocalize-i18next/static/media/getFetch.013f0b90.cjs"
+  },
+  {
+    "revision": "d61f4ace5afb187cb15c6a704fcc10e7",
+    "url": "/simplelocalize-i18next/static/media/logo.d61f4ace.svg"
+  }
+]);
