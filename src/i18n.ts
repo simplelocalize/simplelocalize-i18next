69c22df5573961157a499a62af15d245ab826ecb
Jakub Pomykała <jakub.pomykala@gmail.com>
2021-05-14 16:21:13 +0200

    Update i18n.ts

--- src/i18n.ts
+++ src/i18n.ts
@@ -4,7 +4,7 @@ import LanguageDetector from 'i18next-browser-languagedetector'
 import { initReactI18next } from 'react-i18next'
 
 const projectToken = "5e13e3019cff4dc6abe36009445f0883";
-const loadPath = `https://cdn.simplelocalize.io/${projectToken}/_latest/i18next/{{lng}}/{{ns}}/_index`;
+const loadPath = `https://cdn.simplelocalize.io/${projectToken}/_latest/{{lng}}`;
 
 i18n
   // Enables the i18next backend
@@ -16,9 +16,6 @@ i18n
   .init({
     // Standard language used
     fallbackLng: 'en',
-    debug: true,
-    ns: ["default"],
-    defaultNS: "default",
     //Detects and caches a cookie from the language provided
     detection: {
       order: ['queryString', 'cookie'],
