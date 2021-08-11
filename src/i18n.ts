781c4a8be1b980094cbe80be93cd6e1a80e343ae
jpomykala <jakub.pomykala@gmail.com>
2021-08-11 14:06:09 +0200

    Add option to add missing translations

--- src/i18n.ts
+++ src/i18n.ts
@@ -2,30 +2,44 @@ import i18n from 'i18next'
 import Backend from 'i18next-http-backend'
 import LanguageDetector from 'i18next-browser-languagedetector'
 import { initReactI18next } from 'react-i18next'
+import axios from "axios";
 
-const projectToken = "5e13e3019cff4dc6abe36009445f0883";
-const loadPath = `https://cdn.simplelocalize.io/${projectToken}/_latest/{{lng}}`;
+const fallbackLanguage = 'en'
+
+const projectToken = "5e13e3019cff4dc6abe36009445f0883"; // YOUR PROJECT TOKEN
+const apiKey = ""; // YOUR API KEY
+
+const getTranslationsEndpoint = `https://cdn.simplelocalize.io/${projectToken}/_latest/{{lng}}`;
+const addMissingTranslationsEndpoint = `https://api.simplelocalize.io/api/v1/translations`;
 
 i18n
-  // Enables the i18next backend
   .use(Backend)
-  // Enable automatic language detection
   .use(LanguageDetector)
-  // Enables the hook initialization module
   .use (initReactI18next)
   .init({
-    // Standard language used
-    fallbackLng: 'en',
-    //Detects and caches a cookie from the language provided
-    detection: {
-      order: ['queryString', 'cookie'],
-      cache: ['cookie']
-    },
-    interpolation: {
-      escapeValue: false
-    },
+    fallbackLng: fallbackLanguage,
     backend: {
-      loadPath
+      loadPath: getTranslationsEndpoint
+    },
+    saveMissing: true,
+    missingKeyHandler: (lngs, ns, key, fallbackValue) => {
+
+      const configuration = {
+        headers: {
+          'X-SimpleLocalize-Token': apiKey
+        }
+      };
+
+      const requestBody = {
+        content: [
+          {
+            key: key,
+            language: fallbackLanguage,
+            text: fallbackValue
+          }
+        ]
+      }
+      axios.post(addMissingTranslationsEndpoint, requestBody, configuration)
     }
   })
 
